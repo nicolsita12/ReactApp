a1e0a214e56efb19b647d331c937a3dcba1b1835
nicolsita12 <114593458+nicolsita12@users.noreply.github.com>
2024-03-18 10:49:11 -0500

    asserts, formulario de ingreso, src

--- ReactMovile/App.tsx
+++ ReactMovile/App.tsx
@@ -1,20 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid }
+from 'react-native';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+<View style={styles.container}>
+<Image
+source={require('./assets/chef.jpg')}
+style={styles.imageBackground}
+/>
+<View style={styles.logoContainer}>
+<Image
+source={require('./assets/logo.png')}
+style={styles.logoImage}
+/>
+<Text style={styles.logoText}>FOOD APP</Text>
+</View>
+<View style={styles.form}>
+<Text style={styles.formText}>INGRESAR</Text>
+<View style={styles.formInput}>
+<Image style={styles.formIcon}
+source={require('./assets/email.png')}
+/>
+<TextInput
+style={styles.formTextInput}
+placeholder='Correo electrónico'
+keyboardType='email-address'
+/>
+</View>
+<View style={styles.formInput}>
+<Image style={styles.formIcon}
+source={require('./assets/password.png')}
+/>
+<TextInput
+style={styles.formTextInput}
+placeholder='Contraseña'
+keyboardType='default'
+secureTextEntry={true}
+/>
+</View>
+<View style={{ marginTop: 30 }}>
+<Button
+title='ENTRAR'
+onPress={() => ToastAndroid.show('CLICK', ToastAndroid.LONG)}
+color={'orange'}
+/>
+</View>
+<View style={styles.formRegister}>
+<Text>¿No tienes cuenta?</Text>
+<Text style={styles.formRegisterText}>Regístrate</Text>
+</View>
+</View>
+</View>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  const styles = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: 'black',
+},
+imageBackground: {
+width: '100%',
+height: '100%',
+opacity: 0.7,
+bottom: '30%',
+},
+form: {
+width: '100%',
+height: '40%',
+backgroundColor: 'white',
+position: 'absolute',
+bottom: 0,
+borderTopLeftRadius: 40,
+borderTopRightRadius: 40,
+padding: 30,
+},
+formText: {
+fontWeight: 'bold',
+fontSize: 16,
+},
+formIcon: {
+width: 25,
+height: 25,
+marginTop: 5,
+},
+formInput: {
+flexDirection: 'row',
+marginTop: 30,
+},
+formTextInput: {
+flex: 1,
+borderBottomWidth: 1,
+borderBottomColor: '#AAAAAA',
+marginLeft: 15,
+},
+formRegister: {
+flexDirection: 'row',
+justifyContent: 'center',
+marginTop: 30,
+},
+formRegisterText: {
+fontStyle: 'italic',
+color: 'orange',
+borderBottomWidth: 1,
+borderBottomColor: 'orange',
+fontWeight: 'bold',
+marginLeft: 10,
+},
+logoContainer: {
+position: 'absolute',
+alignSelf: 'center',
+top: '15%',
+},
+logoImage: {
+width: 100,
+height: 100,
+},
+logoText: {
+color: 'white',
+textAlign: 'center',
+fontSize: 20,
+marginTop: 10,
+fontWeight: 'bold',
+},
 });
